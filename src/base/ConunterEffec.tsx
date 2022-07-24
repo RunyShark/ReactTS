34f4e6ad3e7a23cc6335ee2be61ff97a4688c00f
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-24 14:17:30 -0500

    Genericos

--- src/base/ConunterEffec.tsx
+++ src/base/ConunterEffec.tsx
@@ -1,11 +1,20 @@
-import { useEffect, useState } from "react";
+import { useState, useRef, useLayoutEffect } from "react";
+import { gsap } from "gsap";
 const MAXIMUN_COUNT: number = 10;
 export const ConunterEffec = () => {
   const [counter, setcounter] = useState(5);
+  const animation = useRef<HTMLHeadingElement>(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (counter < 10) return;
     console.log("%cSe llego al valor maximo", "color:red;");
+
+    const tl = gsap.timeline();
+
+    tl.to(animation.current, { y: -10, duration: 0.2, ease: "ease.out" }).to(
+      animation.current,
+      { y: 0, duration: 1, ease: "bounce.out" }
+    );
   }, [counter]);
 
   const haddleClick = () => {
@@ -14,7 +23,8 @@ export const ConunterEffec = () => {
 
   return (
     <>
-      <h1>Conunet Effect: {counter}</h1>
+      <h1>Conunet Effect: </h1>
+      <h2 ref={animation}>{counter}</h2>
       <button onClick={haddleClick}>1+</button>
       <button onClick={() => setcounter(counter !== 0 ? counter - 1 : 0)}>
         1-
