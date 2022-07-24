c24158bc029b1a8785afa043986798172ac8e93e
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-24 13:47:47 -0500

    Genericos

--- src/base/ConunterBy.tsx
+++ src/base/ConunterBy.tsx
@@ -9,21 +9,20 @@ interface CounterState {
 }
 
 export const ConunterBy = ({ value = 0 }: Props) => {
-  const [counter, setcounter] = useState<CounterState>({
+  const [{ counter, clicks }, setcounter] = useState<CounterState>({
     counter: value,
     clicks: 0,
   });
-  const { counter: counterTag, clicks } = counter;
 
   const handleClick = (value: number) => {
-    setcounter(({ counter, clicks }) => ({
+    setcounter(() => ({
       counter: counter + value,
       clicks: clicks + 1,
     }));
   };
   return (
     <>
-      <h1>Counter:{counterTag}</h1>
+      <h1>Counter:{counter}</h1>
       <h1>click:{clicks}</h1>
       <button onClick={() => handleClick(1)}>1+</button>
       <button onClick={() => handleClick(5)}>5+</button>
